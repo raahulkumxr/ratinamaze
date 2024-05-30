2146728e122e4dad83cc1c97c03329bf4b622713
raahulkumxr <raahul160205@gmail.com>
2024-05-30 23:46:38 +0530

    started aliter

---
+++ Ratmaze.java
@@ -0,0 +1,15 @@
+public class Ratmaze{
+    class point{
+        int x;
+        int y;
+    }
+    class node{
+        point p;
+        node(int x, int y){
+            p.x = x;
+            p.y = y;
+        }
+    }
+    Ratmaze(){
+        
+    }
